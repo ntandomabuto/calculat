b30ddfb731feef6a6795d329593d1f23001f87af
ntandomabuto <ntandomabuto@gmail.com>
2024-05-17 15:22:07 +0200

    calculator

---
+++ script/calculator.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let input = document.querySelector('.input');
+    let buttons = document.querySelectorAll('button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const action = button.getAttribute('data-action');
+            const value = button.getAttribute('value');
+            if (!action) {
+                input.value += value;
+            } else if (action === 'clear') {
+                if (value === 'CE') {
+                    input.value = '';
+                } else {
+                    input.value = input.value.slice(0, -1);
+                }
+            } else if (action === 'delete') {
+                input.value = '';
+            } else if (action === 'equal') {
+                try {
+                    input.value = eval(input.value);
+                } catch (error) {
+                    input.value = 'Error';
+                }
+            }
+        });
+    });
+});
+
+
+
+
+
+
+
+
